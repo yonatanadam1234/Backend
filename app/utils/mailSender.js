d3742c6f20cd0464f321adb608010158ffdd939d
jbh105 <jaydipbhayani105@gmail.com>
2024-04-25 09:55:56 +0530

    added HMTP

--- app/utils/mailSender.js
+++ app/utils/mailSender.js
@@ -1,29 +1,33 @@
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 465,
+    secure: true, 
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASS, 
+    },
+});
+
+const sendEmail = async (mailOptions) => {
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.error("Failed to send email:", error);
+    }
+};
+
 const MailSender = (sendMailObject) => {
-
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_ID,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
-
     let mailOptions = {
-        from: "Email Id",
+        from: process.env.EMAIL_ID, 
         to: sendMailObject.email,
         subject: sendMailObject.subject,
         text: sendMailObject.text,
         html: sendMailObject.html
     };
-    transporter.sendMail(mailOptions, async (error, info) => {
-        if (error) {
-            throw res.status(400).send({ error, message: "Failed to send OTP" });
-        }
-    })
-}
+    sendEmail(mailOptions);
+};
 
 module.exports = MailSender;
